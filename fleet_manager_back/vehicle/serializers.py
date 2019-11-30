e5de2d0c5bf662e6f8c6c734441433dde434e599
Oyee James <engjames256@gmail.com>
2019-11-30 22:20:56 +0300

    Added endpoint for creating vehichle diary

--- fleet_manager_back/vehicle/serializers.py
+++ fleet_manager_back/vehicle/serializers.py
@@ -1,106 +1,181 @@
 from rest_framework import serializers
-from .models import VehicleInsuranceCompany, Vehicle, VehicleClient,VehicleBodyType, VehicleCompanyCode,VehicleConventionType, VehicleCostCenter, VehicleCountry, VehicleCounty, VehicleCurrencyCodes,VehicleDeductability, VehicleFuelType, VehicleGearBox, VehicleInPull, VehicleLocationCode,VehicleMakeCode, VehicleModelCode, VehicleReturnedWorkshop, VehicleStatus, VehicleType, VehicleTyre
+from .models import (
+    VehicleInsuranceCompany,
+    VehicleSummary,
+    Vehicle,
+    VehicleClient,
+    VehicleBodyType,
+    VehicleCompanyCode,
+    VehicleConventionType,
+    VehicleCostCenter,
+    VehicleCountry,
+    VehicleCounty,
+    VehicleCurrencyCodes,
+    VehicleDeductability,
+    VehicleFuelType,
+    VehicleGearBox,
+    VehicleInPull,
+    VehicleLocationCode,
+    VehicleMakeCode,
+    VehicleModelCode,
+    VehicleReturnedWorkshop,
+    VehicleStatus,
+    VehicleType,
+    VehicleTyre,
+    VehicleBasic,
+    VehicleDiary,
+    VehicleInsuranceCompany
+)
 
 class VehicleInsuranceCompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = VehicleInsuranceCompany
-        fields = '__all__'
-class VehicleTyreSerializer(serializers.ModelSerializer):
+        fields = "__all__"
+class VehicleDiarySerializer(serializers.ModelSerializer):
     class Meta:
-        model = VehicleTyre
-        fields = '__all__'
+        model = VehicleDiary
+        fields = "__all__"
 
-class VehicleTypeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VehicleType
-        fields = '__all__'
-
-class VehicleStatusSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VehicleStatus
-        fields = '__all__'
-        
-class VehicleReturnedWorkshopSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VehicleReturnedWorkshop
-        fields = '__all__'
-
-class VehicleModelCodeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VehicleModelCode
-        fields = '__all__'
-
-class VehicleMakeCodeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VehicleMakeCode
-        fields = '__all__'
-
-class VehicleLocationCodeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VehicleLocationCode
-        fields = '__all__'
-
-class VehicleInPullSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VehicleInPull
-        fields = '__all__'
-
-class VehicleGearBoxSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VehicleGearBox
-        fields = '__all__'
-
-class VehicleFuelTypeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VehicleFuelType
-        fields = '__all__'
-
-class VehicleDeductabilitySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VehicleDeductability
-        fields = '__all__'
-
-class VehicleCurrencyCodesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VehicleCurrencyCodes
-        fields = '__all__'
-
-class VehicleCountySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VehicleCounty
-        fields = '__all__'
-
-class VehicleCountrySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VehicleCountry
-        fields = '__all__'
-
-class VehicleCostCenterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VehicleCostCenter
-        fields = '__all__'
-
-class VehicleConventionTypeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VehicleConventionType
-        fields = '__all__'
 
 class VehicleCompanyCodeSerializer(serializers.ModelSerializer):
     class Meta:
         model = VehicleCompanyCode
-        fields = '__all__'
+        fields = "__all__"
+
+
+class VehicleBasicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VehicleBasic
+        fields = "__all__"
+
+
+class VehicleSummarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VehicleSummary
+        fields = "__all__"
+
+
+class VehicleInsuranceCompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VehicleInsuranceCompany
+        fields = "__all__"
+
+
+class VehicleTyreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VehicleTyre
+        fields = "__all__"
+
+
+class VehicleTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VehicleType
+        fields = "__all__"
+
+
+class VehicleStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VehicleStatus
+        fields = "__all__"
+
+
+class VehicleReturnedWorkshopSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VehicleReturnedWorkshop
+        fields = "__all__"
+
+
+class VehicleModelCodeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VehicleModelCode
+        fields = "__all__"
+
+
+class VehicleMakeCodeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VehicleMakeCode
+        fields = "__all__"
+
+
+class VehicleLocationCodeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VehicleLocationCode
+        fields = "__all__"
+
+
+class VehicleInPullSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VehicleInPull
+        fields = "__all__"
+
+
+class VehicleGearBoxSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VehicleGearBox
+        fields = "__all__"
+
+
+class VehicleFuelTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VehicleFuelType
+        fields = "__all__"
+
+
+class VehicleDeductabilitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VehicleDeductability
+        fields = "__all__"
+
+
+class VehicleCurrencyCodesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VehicleCurrencyCodes
+        fields = "__all__"
+
+
+class VehicleCountySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VehicleCounty
+        fields = "__all__"
+
+
+class VehicleCountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VehicleCountry
+        fields = "__all__"
+
+
+class VehicleCostCenterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VehicleCostCenter
+        fields = "__all__"
+
+
+class VehicleConventionTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VehicleConventionType
+        fields = "__all__"
+
+
+class VehicleCompanyCodeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VehicleCompanyCode
+        fields = "__all__"
+
 
 class VehicleBodyTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = VehicleBodyType
-        fields = '__all__'
+        fields = "__all__"
+
 
 class VehicleClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = VehicleClient
-        fields = '__all__'
+        fields = "__all__"
+
 
 class VehicleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vehicle
-        fields = '__all__'
+        fields = "__all__"
--- fleet_manager_back/vehicle/views.py
+++ fleet_manager_back/vehicle/views.py
@@ -1,108 +1,202 @@
 from rest_framework import viewsets
-from .models import VehicleInsuranceCompany, Vehicle,VehicleClient, VehicleBodyType, VehicleConventionType, VehicleCostCenter, VehicleCompanyCode, VehicleCountry, VehicleCounty, VehicleCurrencyCodes,VehicleDeductability, VehicleFuelType, VehicleGearBox, VehicleInPull, VehicleLocationCode,VehicleMakeCode, VehicleModelCode, VehicleReturnedWorkshop, VehicleStatus, VehicleType,VehicleTyre
-from .serializers import VehicleInsuranceCompanySerializer, VehicleSerializer, VehicleClientSerializer, VehicleBodyTypeSerializer, VehicleCompanyCodeSerializer, VehicleConventionTypeSerializer, VehicleCostCenterSerializer, VehicleCountrySerializer, VehicleCountySerializer, VehicleCurrencyCodesSerializer, VehicleDeductabilitySerializer, VehicleFuelTypeSerializer, VehicleGearBoxSerializer, VehicleInPullSerializer, VehicleLocationCodeSerializer, VehicleMakeCodeSerializer, VehicleModelCodeSerializer, VehicleReturnedWorkshopSerializer, VehicleStatusSerializer, VehicleTypeSerializer, VehicleTyreSerializer
-from rest_framework.permissions import IsAuthenticated 
+from .models import (
+    VehicleInsuranceCompany,
+    Vehicle,
+    VehicleClient,
+    VehicleBodyType,
+    VehicleConventionType,
+    VehicleCostCenter,
+    VehicleCompanyCode,
+    VehicleCountry,
+    VehicleCounty,
+    VehicleCurrencyCodes,
+    VehicleDeductability,
+    VehicleFuelType,
+    VehicleGearBox,
+    VehicleInPull,
+    VehicleLocationCode,
+    VehicleMakeCode,
+    VehicleModelCode,
+    VehicleReturnedWorkshop,
+    VehicleStatus,
+    VehicleType,
+    VehicleTyre,
+    VehicleSummary,
+    VehicleBasic,
+    VehicleDiary,
+    VehicleInsuranceCompany
+)
+from .serializers import (
+    VehicleInsuranceCompanySerializer,
+    VehicleSerializer,
+    VehicleClientSerializer,
+    VehicleBodyTypeSerializer,
+    VehicleCompanyCodeSerializer,
+    VehicleConventionTypeSerializer,
+    VehicleCostCenterSerializer,
+    VehicleCountrySerializer,
+    VehicleCountySerializer,
+    VehicleCurrencyCodesSerializer,
+    VehicleDeductabilitySerializer,
+    VehicleFuelTypeSerializer,
+    VehicleGearBoxSerializer,
+    VehicleInPullSerializer,
+    VehicleLocationCodeSerializer,
+    VehicleMakeCodeSerializer,
+    VehicleModelCodeSerializer,
+    VehicleReturnedWorkshopSerializer,
+    VehicleStatusSerializer,
+    VehicleTypeSerializer,
+    VehicleTyreSerializer,
+    VehicleSummarySerializer,
+    VehicleBasicSerializer, 
+    VehicleDiarySerializer,
+    VehicleInsuranceCompanySerializer
+)
+from rest_framework.permissions import IsAuthenticated
 
 class VehicleInsuranceCompanyView(viewsets.ModelViewSet):
-    permission_class = [IsAuthenticated ]
+    permission_class = [IsAuthenticated]
     queryset = VehicleInsuranceCompany.objects.all()
     serializer_class = VehicleInsuranceCompanySerializer
+
+
+class VehicleBasicView(viewsets.ModelViewSet):
+    permission_class = [IsAuthenticated]
+    queryset = VehicleBasic.objects.all()
+    serializer_class = VehicleBasicSerializer
+
+class VehicleDiaryView(viewsets.ModelViewSet):
+    permission_class = [IsAuthenticated]
+    queryset = VehicleDiary.objects.all()
+    serializer_class = VehicleDiarySerializer
+
+class VehicleSummaryView(viewsets.ModelViewSet):
+    permission_class = [IsAuthenticated]
+    queryset = VehicleSummary.objects.all()
+    serializer_class = VehicleSummarySerializer
+
+
+class VehicleInsuranceCompanyView(viewsets.ModelViewSet):
+    permission_class = [IsAuthenticated]
+    queryset = VehicleInsuranceCompany.objects.all()
+    serializer_class = VehicleInsuranceCompanySerializer
+
+
 class VehicleTyreView(viewsets.ModelViewSet):
-    permission_class = [IsAuthenticated ]
+    permission_class = [IsAuthenticated]
     queryset = VehicleTyre.objects.all()
     serializer_class = VehicleTyreSerializer
 
+
 class VehicleTypeView(viewsets.ModelViewSet):
-    permission_class = [IsAuthenticated ]
+    permission_class = [IsAuthenticated]
     queryset = VehicleType.objects.all()
     serializer_class = VehicleTypeSerializer
 
+
 class VehicleStatusView(viewsets.ModelViewSet):
-    permission_class = [IsAuthenticated ]
+    permission_class = [IsAuthenticated]
     queryset = VehicleStatus.objects.all()
     serializer_class = VehicleStatusSerializer
 
 class VehicleReturnedWorkshopView(viewsets.ModelViewSet):
-    permission_class = [IsAuthenticated ]
+    permission_class = [IsAuthenticated]
     queryset = VehicleReturnedWorkshop.objects.all()
     serializer_class = VehicleReturnedWorkshopSerializer
 
+
 class VehicleModelCodeView(viewsets.ModelViewSet):
-    permission_class = [IsAuthenticated ]
+    permission_class = [IsAuthenticated]
     queryset = VehicleModelCode.objects.all()
     serializer_class = VehicleModelCodeSerializer
 
+
 class VehicleMakeCodeView(viewsets.ModelViewSet):
-    permission_class = [IsAuthenticated ]
+    permission_class = [IsAuthenticated]
     queryset = VehicleMakeCode.objects.all()
     serializer_class = VehicleMakeCodeSerializer
 
+
 class VehicleLocationCodeView(viewsets.ModelViewSet):
-    permission_class = [IsAuthenticated ]
+    permission_class = [IsAuthenticated]
     queryset = VehicleLocationCode.objects.all()
     serializer_class = VehicleLocationCodeSerializer
 
+
 class VehicleInPullView(viewsets.ModelViewSet):
-    permission_class = [IsAuthenticated ]
+    permission_class = [IsAuthenticated]
     queryset = VehicleInPull.objects.all()
     serializer_class = VehicleInPullSerializer
 
+
 class VehicleGearBoxView(viewsets.ModelViewSet):
-    permission_class = [IsAuthenticated ]
+    permission_class = [IsAuthenticated]
     queryset = VehicleGearBox.objects.all()
     serializer_class = VehicleGearBoxSerializer
 
+
 class VehicleFuelTypeView(viewsets.ModelViewSet):
-    permission_class = [IsAuthenticated ]
+    permission_class = [IsAuthenticated]
     queryset = VehicleFuelType.objects.all()
     serializer_class = VehicleFuelTypeSerializer
 
+
 class VehicleDeductabilityView(viewsets.ModelViewSet):
-    permission_class = [IsAuthenticated ]
+    permission_class = [IsAuthenticated]
     queryset = VehicleDeductability.objects.all()
     serializer_class = VehicleDeductabilitySerializer
 
+
 class VehicleCurrencyCodesView(viewsets.ModelViewSet):
-    permission_class = [IsAuthenticated ]
+    permission_class = [IsAuthenticated]
     queryset = VehicleCurrencyCodes.objects.all()
     serializer_class = VehicleCurrencyCodesSerializer
 
+
 class VehicleCountyView(viewsets.ModelViewSet):
-    permission_class = [IsAuthenticated ]
+    permission_class = [IsAuthenticated]
     queryset = VehicleCounty.objects.all()
     serializer_class = VehicleCountySerializer
 
+
 class VehicleCountryView(viewsets.ModelViewSet):
-    permission_class = [IsAuthenticated ]
+    permission_class = [IsAuthenticated]
     queryset = VehicleCountry.objects.all()
     serializer_class = VehicleCountrySerializer
 
+
 class VehicleCostCenterView(viewsets.ModelViewSet):
-    permission_class = [IsAuthenticated ]
+    permission_class = [IsAuthenticated]
     queryset = VehicleCostCenter.objects.all()
     serializer_class = VehicleCostCenterSerializer
 
+
 class VehicleConventionTypeView(viewsets.ModelViewSet):
-    permission_class = [IsAuthenticated ]
+    permission_class = [IsAuthenticated]
     queryset = VehicleConventionType.objects.all()
     serializer_class = VehicleConventionTypeSerializer
 
+
 class VehicleCompanyCodeView(viewsets.ModelViewSet):
-    permission_class = [IsAuthenticated ]
+    permission_class = [IsAuthenticated]
     queryset = VehicleCompanyCode.objects.all()
     serializer_class = VehicleCompanyCodeSerializer
 
+
 class VehicleBodyTypeView(viewsets.ModelViewSet):
-    permission_class = [IsAuthenticated ]
+    permission_class = [IsAuthenticated]
     queryset = VehicleBodyType.objects.all()
     serializer_class = VehicleBodyTypeSerializer
 
+
 class VehicleClientView(viewsets.ModelViewSet):
-    permission_class = [IsAuthenticated ]
+    permission_class = [IsAuthenticated]
     queryset = VehicleClient.objects.all()
     serializer_class = VehicleClientSerializer
 
+
 class VehicleView(viewsets.ModelViewSet):
-    permission_class = [IsAuthenticated ]
+    permission_class = [IsAuthenticated]
     queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
